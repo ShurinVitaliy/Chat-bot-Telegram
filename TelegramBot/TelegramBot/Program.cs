c7d2193abd1646513794d9ef5254aac5be2bc4bd
ShurinVitaliy <34574399+ShurinVitaliy@users.noreply.github.com>
2018-06-09 23:28:04 +0400

    botgetmessage

---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static TelegramBotClient Bot;
+
+        static void Main(string[] args)
+        {
+            Bot = new TelegramBotClient("580295528:AAE9c5hJk__odw6Nd5qNhta5zAedky74w3A");
+
+            Bot.OnMessage += BotOnMessageReceived;
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.WriteLine(me.FirstName);
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void BotOnMessageReceived(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            String name = $"{message.From.FirstName} {message.From.LastName}";
+
+            Console.WriteLine($"{name} Отправил сообщение: {message.Text}"); 
+        }
+    }
+}
