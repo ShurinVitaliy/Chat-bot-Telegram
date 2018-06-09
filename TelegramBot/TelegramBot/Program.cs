ae3ba08bca32e515906f8955f44f19a4b7e2ad9f
ShurinVitaliy <34574399+ShurinVitaliy@users.noreply.github.com>
2018-06-09 23:52:21 +0400

    switch message

--- TelegramBot/TelegramBot/Program.cs
+++ TelegramBot/TelegramBot/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
 
 namespace TelegramBot
 {
@@ -17,6 +18,7 @@ namespace TelegramBot
             Bot = new TelegramBotClient("580295528:AAE9c5hJk__odw6Nd5qNhta5zAedky74w3A");
 
             Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
 
             var me = Bot.GetMeAsync().Result;
 
@@ -27,12 +29,37 @@ namespace TelegramBot
             Bot.StopReceiving();
         }
 
-        private static void BotOnMessageReceived(object sender, MessageEventArgs e)
+        private static void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs e)
         {
             var message = e.Message;
+
+            if (message == null || message.Type != MessageType.Text)
+                return;
             String name = $"{message.From.FirstName} {message.From.LastName}";
 
             Console.WriteLine($"{name} Отправил сообщение: {message.Text}"); 
+
+            switch (message.Text)
+            {
+                case "/start":
+                    string text = @"Список команд : 
+/start - запуск бота 
+/inline - вывод меню 
+/keyboard - вывод клавиатуры";
+                    await Bot.SendTextMessageAsync(message.From.Id, text);
+                    break;
+                case "/inline":
+                    break;
+                case "/keyboard":
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
